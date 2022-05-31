b6bded940ab4bf403f312c37240ff35cab931d8a
joselgmont <joseluis.gomesmont@hotmail.com>
2022-05-30 22:24:23 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,20 @@ import '../style.css';
 import Card from './Card';
 import pokemonList from '../pokemon';
 
-
-
-
-
 function App() {
   return (
     <div>
       <h1><span>Pokedex - React</span></h1>
       <div className="cards">
-        {pokemonList.map((card) => <Card 
-      key = {card.key}
-      nome = {card.nome}
-      dexNum = {card.dexNum}
-      imagemCard = {card.imagemCard}
-      descricao = {card.descricao}
-      tipo = {card.tipo}
-    />)}
+        {pokemonList.map((card) => 
+        <Card 
+          key = {card.key}
+          nome = {card.nome}
+          dexNum = {card.dexNum}
+          imagemCard = {card.imagemCard}
+          descricao = {card.descricao}
+          tipo = {card.tipo}
+        />)}
       </div>
     </div>
   );
